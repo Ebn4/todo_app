6b963f032a9e309a1583b7dabe9dbc20cfa70bec
Exauce Bodi <exaucebodi4@gmail.com>
2025-06-30 15:10:44 +0200

    Initialisation de get_storage et injection des service local

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:todo_app/business/service/service01.dart';
+import 'package:todo_app/business/service/serviceLocal.dart';
 import 'package:todo_app/implementation/implementation.dart';
+import 'package:todo_app/implementation/implementationLocal.dart';
 import 'package:todo_app/pages/home/homepage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:get_storage/get_storage.dart';
 
 GetIt getIt = GetIt.instance;
 setup() {
   getIt.registerLazySingleton<Service01>(() => Implementation());
+  getIt.registerLazySingleton<Servicelocal>(() => ImplementationLocal(box: GetStorage()));
 }
 
 void main() async {
   setup(); // appel setup function to register services
+  await GetStorage.init(); // Initialize GetStorage for local storage
   await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }

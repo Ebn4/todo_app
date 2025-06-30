7ea483f36dc715a9e2ef280b6d6180f90bfc6905
Exauce Bodi <exaucebodi4@gmail.com>
2025-06-30 14:46:34 +0200

    Initialisation des geit et injections des services

---
+++ lib/pages/home/homepage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/pages/widgets/alerMessage.dart';
+import 'package:todo_app/pages/widgets/myBottomNavbar.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My todolist',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: SingleChildScrollView(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 130),
+            SizedBox(
+              height: 80,
+              width: 400,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Ajouter une tâche',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: const BorderSide(color: Colors.blue),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(color: Colors.blue),
+                  ),
+                  labelText: 'Ajouter une tâche',
+                  labelStyle: const TextStyle(color: Colors.blue),
+                  prefixIcon: const Icon(Icons.task, color: Colors.blue),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.add, color: Colors.blue),
+                    onPressed: () {
+                      print("I learn python because is awesome");
+                    },
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Column(children: []),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          MessageAlert();
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+      bottomNavigationBar: Mybottomnavbar(),
+    );
+  }
+}
